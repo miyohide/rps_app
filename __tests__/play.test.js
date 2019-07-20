ac9c216f1c7448ac4c44704910ea5e99c7e4750b
Hidenori Miyoshi <miyohide@gmail.com>
2019-07-20 12:39:10 +0900

    create sample app and jtest spec

---
+++ __tests__/play.test.js
@@ -0,0 +1,18 @@
+import {Requests} from '../src/play';
+import { JestEnvironment } from '@jest/environment';
+
+describe('play function', () => {
+    describe('Player 1 wins', () => {
+        const observer = {
+            player1Wins() { console.log("player1Wins"); },
+        };
+        beforeEach(() => {
+            jest.spyOn(observer, 'player1Wins').mockImplementation();
+        });
+
+        it('rock vs scissors', () => {
+            new Requests().play('rock', 'scissors', observer);
+            expect(observer.player1Wins).toBeCalled();
+        });
+    });
+});
---
+++ src/play.js
@@ -0,0 +1,5 @@
+export class Requests {
+    play(player1Hand, player2Hand, observer) {
+        observer.player1Wins();
+    }
+}
