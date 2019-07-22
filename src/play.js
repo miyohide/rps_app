a07ad46092a6e82a17411182c5473d16f6aec6d6
Hidenori Miyoshi <miyohide@gmail.com>
2019-07-22 21:31:37 +0900

    implement player2Wins pattern.

--- src/play.js
+++ src/play.js
@@ -1,5 +1,8 @@
 export class Requests {
     play(player1Hand, player2Hand, observer) {
-        observer.player1Wins();
+        if (player1Hand === 'scissors' && player2Hand === 'rock') {
+            return observer.player2Wins();
+        }
+        return observer.player1Wins();
     }
 }
