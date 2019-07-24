74dd0130afa38a49a600589a7cd52e6830b6c834
Hidenori Miyoshi <miyohide@gmail.com>
2019-07-24 18:24:06 +0900

    fix fail test.

--- src/play.js
+++ src/play.js
@@ -1,6 +1,8 @@
 export class Requests {
     play(player1Hand, player2Hand, observer) {
-        if (player1Hand === 'scissors' && player2Hand === 'rock') {
+        if (
+            (player1Hand === 'scissors' && player2Hand === 'rock') ||
+            (player1Hand === 'rock' && player2Hand === 'paper')) {
             return observer.player2Wins();
         }
         return observer.player1Wins();
