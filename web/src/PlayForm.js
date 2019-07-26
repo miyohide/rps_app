5d2bdfec03a700d10697aca27e3eecdcb70eade0
Hidenori Miyoshi <miyohide@gmail.com>
2019-07-26 21:14:39 +0900

    implement first app

---
+++ web/src/PlayForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class PlayForm extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>RPS App</h1>
+            </div>
+        );
+    }
+}
